36bc85013452af58d85e5b37ab9e5be739c34b25
Lujango <935819994@qq.com>
2016-01-10 22:11:42 +0800

    success cookies

--- cookies_save.py
+++ cookies_save.py
@@ -3,7 +3,8 @@ import cookielib
 import urllib2
 import urllib
 
-# 验证码
+
+# 验证码登陆
 def getlogindata():
     postdata = urllib.urlencode({
             'mobile':'15583581921',
@@ -12,15 +13,16 @@ def getlogindata():
     return postdata
 
 
+# 计划书费率参数
 def getpama():
     postdata = urllib.urlencode({
-            'insuranceTypeId':'168',
-            'sex':'1',
-            'age':"18",
-            'years':"10",
-            'baoe':"20",
-            'baofei':"4113",
-            'idea':"-1",
+            'insuranceTypeId':'168', #险种id
+            'sex':'1',                 #性别
+            'age':"42",               #年龄
+            'years':"10",             #缴费期间
+            'baoe':"10",              #保额
+            'baofei':"4113",          #保费
+            'idea':"-1",              #保险理念
             'cname':"",
             'csex':"1"
         })
@@ -28,9 +30,10 @@ def getpama():
 
 
 if __name__=="__main__":
-    filename = 'cookie.txt'
+    cookiespath = "cookie.txt"
+    filename = cookiespath
     cookie = cookielib.MozillaCookieJar(filename)
-    cookie.load('cookie.txt', ignore_discard=True, ignore_expires=True)
+    cookie.load(cookiespath, ignore_discard=True, ignore_expires=True)
     handler = urllib2.HTTPCookieProcessor(cookie)
     opener = urllib2.build_opener(handler)
     response = opener.open("http://app.winbaoxian.com/planBook/calculate",getpama())
